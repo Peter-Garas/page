e87000ced5790a124cc26f85316c032ff178249b
Peter-Garas <85300784+Peter-Garas@users.noreply.github.com>
2021-08-08 11:52:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function launchProject(type, location) {
   document.getElementById("frame").setAttribute("src", location);
   document.getElementById("staticBackdropLabel").innerHTML = type;
 }
+
+function clearContent() {
+  document.getElementById("frame").setAttribute("src", '');
+}
